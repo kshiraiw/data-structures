fedb56f7e1176d8777d2cc3f1685547209b72a3f
Hack Reactor Students <accounts@hackreactor.com>
2015-07-02 11:40:48 -0700

    Working on Binary Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,50 @@
 var BinarySearchTree = function(value){
+	this.root = new Node(value);
+
+	this.insert = function(value) {
+		var tree = new Node(value);
+
+		if(!this.root){
+			this.root = tree;
+		} else {
+			console.log(this.root)
+			var current = this.root;
+			var isPlaced = false;
+			while(!isPlaced) {
+				if(value > current.value){
+					console.log("hello")
+					if(current.right === null){
+						isPlaced = true;
+						current.right = tree;
+					} else {
+						current = current.right;
+					}
+				} else {
+					if (current.left === null) {
+						isPlaced = true;
+						current.left = tree;
+					} else {
+						current = current.left;
+					}
+				}
+			}
+		}
+	};
+	this.contains = function(value) {
+
+	};
+	this.depthFirstLog = function(callback) {
+
+	};
+
+
 };
+
+var Node = function(value) {
+	this.value = value;
+	this.right = null;
+	this.left = null;
+}
 
 
 /*
