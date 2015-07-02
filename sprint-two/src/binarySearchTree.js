c3c646a47a876f5adddf818bf7c8d134e21f90f8
Hack Reactor Students <accounts@hackreactor.com>
2015-07-02 16:14:16 -0700

    Completed binary

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,44 +1,58 @@
 var BinarySearchTree = function(value){
 	this.root = new Node(value);
+};
 
-	this.insert = function(value) {
+BinarySearchTree.prototype = {
+	insert: function(value){
 		var tree = new Node(value);
-
-		if(!this.root){
-			this.root = tree;
-		} else {
-			console.log(this.root)
-			var current = this.root;
-			var isPlaced = false;
-			while(!isPlaced) {
-				if(value > current.value){
-					console.log("hello")
-					if(current.right === null){
-						isPlaced = true;
-						current.right = tree;
-					} else {
-						current = current.right;
-					}
+		var current = this.root;
+		var isPlaced = false;
+		while(!isPlaced) {
+			if(value > current.value){
+				if(current.right === null){
+					isPlaced = true;
+					current.right = tree;
 				} else {
-					if (current.left === null) {
-						isPlaced = true;
-						current.left = tree;
-					} else {
-						current = current.left;
-					}
+					current = current.right;
+				}
+			} else {
+				if (current.left === null) {
+					isPlaced = true;
+					current.left = tree;
+				} else {
+					current = current.left;
 				}
 			}
 		}
-	};
-	this.contains = function(value) {
-
-	};
-	this.depthFirstLog = function(callback) {
-
-	};
-
-
+	},
+	contains: function(value) {
+		var isFound = false;
+		var current = this.root;
+		while (!isFound && current !== null) {
+			if (value === current.value) {
+				isFound = true;
+			} else if (value > current.value) {
+				current = current.right;
+			} else {
+				current = currrent.left;
+			}
+		}
+		return isFound;
+	},
+	depthFirstLog: function(callback){
+		var addCallback = function(current){
+			callback(current.value);
+			if(current.left){
+				addCallback(current.left);
+			}
+			if(current.right){
+				addCallback(current.right);
+			}
+		}
+		addCallback(this.root);
+	}
 };
+
 
 var Node = function(value) {
 	this.value = value;
